47c6ac58c584e81b4c0cd93c7e6fe8012094fe51
jalambert98 <97771210+jalambert98@users.noreply.github.com>
2022-01-16 21:53:31 -0800

    Updated for input capture test
    Revised test sketch utilizes a single ping sensor on the ICP1 pin and is able to accurately report (via Serial.print statements) distance measurements at 100Hz.

--- JSN_Test/JSN_Test.ino
+++ JSN_Test/JSN_Test.ino
@@ -14,37 +14,31 @@
 #define BAUDRATE        115200
 #define NUM_SENSORS     3
 
-#define TRIG1           0x05
-#define TRIG2           0x06
-#define TRIG3           0x08
-#define ECHO1           0x02
-#define ECHO2           0x03
-#define ECHO3           0x07
+#define TRIG1           0x07
+#define ECHO1           0x04
 
-JSN_Sensor *sens[3];
+JSN_Sensor *sens;
 uint8_t i;
+unsigned int dist;
   
 void setup() {
-  sens[0] = new JSN_Sensor(TRIG1, ECHO1);
-  sens[1] = new JSN_Sensor(TRIG2, ECHO2);
-  sens[2] = new JSN_Sensor(TRIG3, ECHO3);
+  sens = new JSN_Sensor(TRIG1, ECHO1);
   
   // Begin serial comms over UART
   Serial.begin(BAUDRATE);
 
   // Initialize interrupt hardware associated with JSN sensors
-  JSN_Sensor::Init(EXTERNAL_INTERRUPTS);
+  JSN_Sensor::Init(INPUT_CAPTURE);
 }
 
 void loop() {
-  for(i=0; i<3; i++) {
-    sens[i]->Trig();
-    delay(250);
-    Serial.print("Distance");
-    Serial.print(i+1);
-    Serial.print(" = ");
-    Serial.print(sens[i]->GetDistance());
-    Serial.print("mm\n");
-    delay(250);
-  }
+  sens->Trig();
+  delay(10);
+  dist = sens->GetDistance();
+  Serial.print("Distance");
+  Serial.print(i+1);
+  Serial.print(" = ");
+  Serial.print(dist);
+  Serial.print("mm\n");
+  delay(10);
 }
