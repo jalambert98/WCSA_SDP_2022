1d5351819919233e03ce605913575eb94627927e
Jack <joalambe@ucsc.edu>
2022-01-30 15:30:51 -0800

    Added detailed comments to JSN library functions + test harness

--- WCSA_SDP_2022.X/JSN_Sensor.c
+++ WCSA_SDP_2022.X/JSN_Sensor.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 
 
-#define JSN_SENSOR_TEST
+#define JSN_SENSOR_TEST       // toggle comment to enable/disable test harness
 
 //==============================================================================
 //---------------------------- STATICS VARIABLES -------------------------------
@@ -31,7 +31,11 @@ static JSN_t Sens1, Sens2, Sens3;
 //==============================================================================
 
 uint8_t JSN_Sensor_Init(JSN_t *Sensor, PinName_t trigPin, PinName_t echoPin) {
-
+    /*
+     * Need to move JSN_Sensor_Init() calls into this function once
+     * MCU pin config is finalized
+     */
+    
     // Set echoPin as INPUT if it is one of the following: [A2,A4,C3,C5]
     switch (echoPin) {
         case A2:
@@ -41,6 +45,7 @@ uint8_t JSN_Sensor_Init(JSN_t *Sensor, PinName_t trigPin, PinName_t echoPin) {
             SetPin(echoPin, INPUT);
             break;
         default:
+            printf("ERROR: Invalid echoPin (must be [A2, A4, C3, C5])");
             return ERROR;
     }
 
@@ -63,6 +68,7 @@ uint8_t JSN_Sensor_Init(JSN_t *Sensor, PinName_t trigPin, PinName_t echoPin) {
             WritePin(trigPin, LOW);
             break;
         default:
+            printf("ERROR: Invalid trigPin (must NOT be [A2, A3, A4, C3, C5])");
             return ERROR;
     }
 
@@ -79,9 +85,6 @@ uint8_t JSN_Sensor_Init(JSN_t *Sensor, PinName_t trigPin, PinName_t echoPin) {
 
 //------------------------------------------------------------------------------
 
-/*
- *  EXPECTS that FRT_Init() has already been called!!
- */
 void JSN_Sensor_Trig(JSN_t *Sensor) {
     micros = FRT_GetMicros();
 
@@ -124,39 +127,62 @@ JSN_t* JSN_GetLastTrig(void) {
 
 #ifdef JSN_SENSOR_TEST
 
+// DevBoard pin designators for test harness execution
+#define TRIG1               C6
+#define TRIG2               A1
+#define ECHO1               C5
+#define ECHO2               C3
+
 #define SAMPLE_PERIOD       50  // Sensor reading occus every [x]ms
 #define MIN_DIST_LED        500 // Turn LED on if object within [x]mm of sensor
+
 
 int main(void) {
     // Initialize required libraries
     PIC16_Init();
-    JSN_Sensor_Init(&Sens1, C6, C5);
-    JSN_Sensor_Init(&Sens2, A1, C3);
+    
+    /*
+     * If invalid trig/echoPin, Init() function will print error statement
+     * & this conditional statement will halt further program execution
+     */
+    if(JSN_Sensor_Init(&Sens1, TRIG1, ECHO1) == ERROR)
+        while(1);
+    if(JSN_Sensor_Init(&Sens2, TRIG2, ECHO2) == ERROR)
+        while(1);
 
     // Initialize function variables
-    unsigned long currMilli = 0;
-    unsigned long prevMilli = 0;
-    uint8_t i = 1;
+    unsigned long currMilli = FRT_GetMillis();  // initial timer reading
+    unsigned long prevMilli = currMilli;        // before event->(prev == curr)
+    uint8_t i = 1;                              // read Sens1 first
+    
+    // Initialize LED as OUTPUT LOW
     SetPin(C0, OUTPUT);
     WritePin(C0, LOW);
 
-    JSN_Sensor_Trig(&Sens1);
+    /*
+     * Initially trigger Sens2 in order to prepare readings from
+     * BOTH sensors after the first TRIG cycle
+     */
     JSN_Sensor_Trig(&Sens2);
-
+  
     while (1) {
+        // Continuously update free running timer (milliseconds)
         currMilli = FRT_GetMillis();
 
-        // Trigger a sensor every SAMPLE_PERIOD milliseconds
-        if ((currMilli - prevMilli) >= SAMPLE_PERIOD) {
+        // Trigger a sensor reading every SAMPLE_PERIOD milliseconds
+        if ((currMilli - prevMilli) >= SAMPLE_PERIOD) {            
+            /*
+             * Alternate sensor to trig each cycle 
+             * (each sensor reads every [2*SAMPLE_PERIOD])
+             */
             switch (i) {
                 case 1:
-                    // send new TRIG signal*/
+                    // Trigger a reading from Sens1
                     JSN_Sensor_Trig(&Sens1);
                     i = 2;
                     break;
-
                 case 2:                    
-                    // send new TRIG signal*/
+                    // Trigger a reading from Sens2
                     JSN_Sensor_Trig(&Sens2);
                     i = 1;
                     break;
