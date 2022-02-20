698c8be17eb62e8c0c047bb3882f6a0917eebb20
Jack <joalambe@ucsc.edu>
2022-02-20 15:21:12 -0800

    Skeleton code for main application file

---
+++ WCSA_SDP_2022.X/build/default/production/WCSA_MainApp.p1.d
@@ -0,0 +1,21 @@
+build/default/production/WCSA_MainApp.p1:  \
+WCSA_MainApp.c  \
+PIC16Xpress_DevBoard.h  \
+mcc.h  \
+device_config.h  \
+pin_manager.h  \
+interrupt_manager.h  \
+tmr3.h  \
+tmr1.h  \
+tmr2.h  \
+pwm5.h  \
+tmr0.h  \
+ccp4.h  \
+ccp2.h  \
+ccp3.h  \
+ccp1.h  \
+eusart.h  \
+FRT.h  \
+JSN_Sensor.h  \
+SpeakerTone.h  \
+MotorControl.h 
